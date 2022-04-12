630a2fd6ea4525d5144a36bfc76680c3768dff78
patzick <13100280+patzick@users.noreply.github.com>
2022-04-12 23:13:33 +0200

    chore: remove old code

--- vuepress-theme-shopware-docs/plugin.js
+++
@@ -1,6 +0,0 @@
-const { path } = require("@vuepress/shared-utils");
-
-module.exports = (options = {}, context) => ({
-  name: "tailwind-postcss-plugin",
-  enhanceAppFiles: path.resolve(__dirname, "tailwindPostcssScript.js"),
-});
