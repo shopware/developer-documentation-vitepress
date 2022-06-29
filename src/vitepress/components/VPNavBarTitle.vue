1970b0319a0ff43c555e35400311dfcb0a3a83af
Sebastian Seggewiss <s.seggewiss@shopware.com>
2022-06-29 09:52:22 +0200

    Use withBase for navbar links

--- src/vitepress/components/VPNavBarTitle.vue
+++ src/vitepress/components/VPNavBarTitle.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="flex items-center">
-    <a class="VPNavBarTitle" href="/">
+    <a class="VPNavBarTitle" :href="withBase('/')">
       <img src="./../assets/shopware-docs.svg" alt="Shopware" class="h-40px" />
     </a>
-    <a href="/installation.html"><span class="accent ml-5 text-xl">EA</span></a>
+    <a :href="withBase('/installation.html')"><span class="accent ml-5 text-xl">EA</span></a>
   </div>
 </template>
+
+<script setup>
+import {withBase} from "vitepress";
+</script>
 
 <style scoped>
 .VPNavBarTitle {
