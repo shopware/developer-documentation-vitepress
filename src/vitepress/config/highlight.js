c22318d549db8ddf53a8fab9c0fd254087d06b1d
Maciek Kucmus <maciejkucmus@gmail.com>
2022-08-29 12:05:40 +0200

    fix: code highlighting and copying function (#9)

--- src/vitepress/config/highlight.js
+++ src/vitepress/config/highlight.js
@@ -10,15 +10,45 @@ function escapeHtml(html) {
   return html.replace(/[&<>"']/g, (chr) => htmlEscapes[chr])
 }
 
+function range(size, startAt = 0) {
+  return [...Array(size).keys()].map(i => i + startAt);
+}
+
+/**
+ * Translate the "1,2,6-8" into an array of numbers to be like: 1,2,6,7,8
+ */
+function getHighlightLinesConfig(linesConfig = '') {
+  if (!linesConfig) {
+    return;
+  }
+
+  let linesToHighlight = [];
+  const linesGroups = linesConfig.split(',')
+  linesGroups.forEach(group => {
+    const rangeParts = group.split('-')
+    if (rangeParts.length === 1) {
+      linesToHighlight.push(parseInt(rangeParts[0]))
+    } else {
+      const start = parseInt(rangeParts[0])
+      const end = parseInt(rangeParts[1])
+      linesToHighlight = linesToHighlight.concat(range(end - start + 1, start))
+    }
+  })
+
+  return linesToHighlight;
+}
+
 module.exports = async (theme = 'material-palenight') => {
   const highlighter = await require('shiki').getHighlighter({
     theme
   })
 
-  return (code, lang) => {
+  return (code, lang, attrs) => {
     if (!lang || lang === 'text') {
       return `<pre v-pre><code>${escapeHtml(code)}</code></pre>`
     }
-    return highlighter.codeToHtml(code, lang).replace(/^<pre.*?>/, '<pre v-pre>')
+    const linesConfig = getHighlightLinesConfig(attrs);
+    return highlighter.codeToHtml(code, {lang, lineOptions: linesConfig?.map(line => ({line, classes: ["highlighted"]}))} ).replace(/^<pre.*?>/, '<pre v-pre>')
+
   }
 }
