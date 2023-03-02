795e182bb93845e7011ab9e7a6c63f2502ef170b
Bojan Rajh <b.rajh@shopware.com>
2023-03-02 11:30:42 +0100

    Refetch on route change

--- src/shopware/components/SwagRelatedArticles.vue
+++ src/shopware/components/SwagRelatedArticles.vue
@@ -1,27 +1,27 @@
 <template>
-  <div v-if="articles.length" class="vt-doc">
+  <div v-if="articles.length" class="vt-doc" :key="route.path">
     <b class="mb-2 flex">Continue with related topics:</b>
     <PageRef
-      v-for="link in articles"
-      :page="link.page"
-      :title="link.title"
-      :sub="link.description"
+        v-for="link in articles"
+        :page="link.page"
+        :title="link.title"
+        :sub="link.description"
     ></PageRef>
   </div>
 </template>
 
 <script async setup>
-import { ref, onMounted } from "vue";
+import {ref, onMounted, watch} from "vue";
 import axios from "axios";
-import { useData, useRoute } from "vitepress";
-import { useConfig } from "../../vitepress/composables/config";
+import {useData, useRoute} from "vitepress";
+import {useConfig} from "../../vitepress/composables/config";
 
 const articles = ref([]);
 const route = useRoute();
-const { config } = useConfig();
+const {config} = useConfig();
 const similarArticlesHost = config.value.swag?.similarArticlesHost;
 
-onMounted(async () => {
+const fetchSimilarArticles = async () => {
   if (!similarArticlesHost) {
     return;
   }
@@ -31,27 +31,33 @@ onMounted(async () => {
       id = `${id}index.md`;
     }
 
-    const { data } = await axios.post(
-      `${similarArticlesHost}/neighbours`,
-      {
-        id: id,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
+    const {data} = await axios.post(
+        `${similarArticlesHost}/neighbours`,
+        {
+          id: id,
         },
-      }
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
     );
-    articles.value = [
-      ...articles.value,
-      ...data.results.map(({ id, heading, description }) => ({
-        page: `/${id.replace(/\.[^/.]+$/, ".html").replace('/index.html', '')}`,
-        title: heading,
-        description: description,
-      })),
-    ];
+    articles.value = data.results.map(({id, heading, description}) => ({
+      page: `/${id.replace(/\.[^/.]+$/, ".html").replace('/index.html', '')}`,
+      title: heading,
+      description: description,
+    }));
   } catch (e) {
     console.error(e);
   }
-});
+};
+
+// can't use immediate: true because of SSR?
+watch(
+    () => route.path,
+    fetchSimilarArticles,
+)
+
+// due to immediate: false
+onMounted(fetchSimilarArticles)
 </script>
