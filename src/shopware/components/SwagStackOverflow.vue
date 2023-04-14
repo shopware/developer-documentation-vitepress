a0258e4bedc1cdba833dac5bd810365c77a048ab
Bojan Rajh <b.rajh@shopware.com>
2023-04-14 09:11:40 +0200

    First step of the official theme

---
+++ src/shopware/components/SwagStackOverflow.vue
@@ -0,0 +1,19 @@
+<template>
+  <p
+      class="edit-link"
+      v-if="theme.editLink && frontmatter.stackOverflowLink !== false"
+  >
+    <!--<VTIconStackOverflow class="vt-icon"/>-->
+    <VTLink :href="'https://stackoverflow.com/questions/ask?tags=shopware'"
+            :no-icon="true">Ask a question on StackOverflow
+    </VTLink>
+  </p>
+</template>
+
+<script setup lang="ts">
+import {useData} from "vitepress";
+import VTIconStackOverflow from "../../core/components/icons/VTIconStackOverflow.vue";
+import VTLink from "../../core/components/VTLink.vue";
+
+const {page, frontmatter, theme} = useData()
+</script>
--- src/vitepress/index.ts
+++ src/vitepress/index.ts
@@ -1,37 +1,72 @@
-// import "tailwindcss/tailwind.css";
-import "uno.css";
-import './styles/index.css'
-import VPApp from './components/VPApp.vue'
-import VPNotFound from './components/VPNotFound.vue'
-import { Theme } from 'vitepress'
-import { withConfigProvider } from './composables/config'
+/**
+ * We are extending from the default theme.
+ */
+import Theme from 'vitepress/theme'
+import {withConfigProvider} from './composables/config'
+import {h} from 'vue'
 
-// import ActionItem from "./../shopware/components/ActionItem.vue";
-// import RegistrationForm from "./../shopware/components/RegistrationForm.vue";
-// import LandingWrapper from "./../shopware/components/LandingWrapper.vue";
-// import TopBar from "./../shopware/components/TopBar.vue";
+
+/**
+ * Component that we want to make globally available.
+ */
 import PageRef from "./../shopware/components/PageRef.vue";
 import YoutubeRef from "./../shopware/components/YoutubeRef.vue";
 import CodeBlock from "./../shopware/components/CodeBlock.vue";
 import Tabs from "./../shopware/components/Tabs.vue";
 import Tab from "./../shopware/components/Tab.vue";
 
-const VPTheme: Theme = {
-  Layout: withConfigProvider(VPApp),
-  NotFound: VPNotFound,
-  enhanceApp({ app }) {
-    // app.component('ActionItem', ActionItem),
-    // app.component('RegistrationForm', RegistrationForm),
-    // app.component('LandingWrapper', LandingWrapper),
-    // app.component('TopBar', TopBar),
-      app.component("YoutubeRef", YoutubeRef),
-      app.component("PageRef", PageRef),
-      app.component("CodeBlock", CodeBlock),
-      app.component("Tabs", Tabs),
-      app.component("Tab", Tab);
-  }
-};
+// broken components
+// import ActionItem from "./../shopware/components/ActionItem.vue";
+// import RegistrationForm from "./../shopware/components/RegistrationForm.vue";
+// import LandingWrapper from "./../shopware/components/LandingWrapper.vue";
+// import TopBar from "./../shopware/components/TopBar.vue";
 
-export { VPTheme }
+/**
+ * Slot contents.
+ */
+import SwagRelatedArticles from "../shopware/components/SwagRelatedArticles.vue";
+import SwagSidebarVersionSwitcher from "../shopware/components/SwagSidebarVersionSwitcher.vue";
+import SwagStackOverflow from "../shopware/components/SwagStackOverflow.vue";
 
-export type { Config } from './config'
+/**
+ * Import styles at the end.
+ */
+import '../shopware/styles/_index.scss'
+import "uno.css";
+
+const SWAGTheme = (myConfig: { enhanceApp?: Function } = {}) => ({
+    Layout: (() => {
+        return h(
+            withConfigProvider(Theme.Layout),
+            null,
+            {
+                'doc-footer-before': () => [
+                    h(SwagRelatedArticles),
+                    h(SwagStackOverflow)
+                ],
+                'sidebar-nav-before': () => h(SwagSidebarVersionSwitcher),
+            }
+        )
+    })(),
+    //NotFound: VPNotFound,
+    enhanceApp({app, router, siteData}) {
+        // app.component('ActionItem', ActionItem),
+        // app.component('RegistrationForm', RegistrationForm),
+        // app.component('LandingWrapper', LandingWrapper),
+        // app.component('TopBar', TopBar),
+        app.component("YoutubeRef", YoutubeRef);
+        app.component("PageRef", PageRef);
+        app.component("CodeBlock", CodeBlock);
+        app.component("Tabs", Tabs);
+        app.component("Tab", Tab);
+
+        /**
+         * Allow extending from sub-apps.
+         */
+        myConfig.enhanceApp?.({app, router, siteData});
+    }
+});
+
+export {SWAGTheme}
+
+export type {Config} from './config'
