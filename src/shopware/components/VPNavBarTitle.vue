9368b5005a32f3c24b66ad085e5c0147955c8569
Bojan Rajh <b.rajh@shopware.com>
2023-04-18 11:27:38 +0200

    Dark theme, logo

---
+++ src/shopware/components/VPNavBarTitle.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex items-center">
+    <a class="VPNavBarTitle" :href="withBase('/')">
+      <img
+          src="../assets/shopware-docs.svg"
+          alt="Shopware"
+          class="VPNavBarImage"
+      />
+    </a>
+    <a :href="withBase('/installation.html')"
+    ><span class="accent text-xl">EA</span></a
+    >
+  </div>
+</template>
+
+<script setup>
+import { withBase } from "vitepress";
+</script>
+
+<style scoped>
+.VPNavBarTitle {
+  display: flex;
+  align-items: center;
+  padding-top: 1px;
+  height: var(--vt-nav-height);
+  color: var(--vt-c-brand);
+  transition: opacity 0.25s;
+}
+
+.VPNavBarImage {
+  width: 90%;
+  height: 40px;
+}
+
+.VPNavBarTitle:hover {
+  opacity: 0.6;
+}
+</style>
--- src/shopware/config/baseConfig.js
+++ src/shopware/config/baseConfig.js
@@ -45,6 +45,7 @@ module.exports = async () => ({
 
       alias: {
         '../composables/edit-link': path.resolve(__dirname, '../composables/edit-link.ts'),
+        './VPNavBarTitle.vue': path.resolve(__dirname, '../components/VPNavBarTitle.vue'),
       }
     },
     // https://www.npmjs.com/package/@rollup/plugin-node-resolve ?
