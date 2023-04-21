e6733d3ec1a255e5722964dfd3668e00caaf5c04
Bojan Rajh <b.rajh@shopware.com>
2023-04-21 13:56:58 +0200

    chore: release 1.0.0-alpha.7

--- src/shopware/components/SwagLandingCard.vue
+++ src/shopware/components/SwagLandingCard.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="SwagLandingCard c-any-card basis-1/3 rounded-lg border-1px border-#eeeeee rounded-md">
-    <a href="#">
-      <img class="rounded-t-lg" :src="image" alt="" />
-    </a>
-    <div class="p-4">
-      <a href="#">
-        <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-          <slot name="title"></slot>
-          {{ title }}
-        </h5>
-      </a>
-      <p class="SwagLandingCard_sub font-normal text-gray-700 dark:text-gray-400 text-xs">
-        <slot name="sub"></slot>
-        {{ sub }}
-      </p>
+    <div class="SwagLandingCard c-any-card basis-1/3 hover:scale-110 rounded-l-[0.375rem] border-1px border-#eeeeee rounded-md">
+        <div class="flex flex-col items-center gap-4">
+            <a href="#">
+                <img class="rounded-t-lg" :src="image" alt=""/>
+            </a>
+            <div class="p-4">
+                <a href="#">
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                        <slot name="title"></slot>
+                        {{ title }}
+                    </h5>
+                </a>
+                <p class="SwagLandingCard_sub font-normal text-gray-700 dark:text-gray-400 text-xs">
+                    <slot name="sub"></slot>
+                    {{ sub }}
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -29,21 +31,21 @@
 
 <script setup lang="ts">
 const props = defineProps({
-  page: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  sub: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  }
+    page: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    sub: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
 });
 </script>
