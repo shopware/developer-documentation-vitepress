fdd70dd54bce050d17aaf8d3b5d9df93e816f15a
Bojan Rajh <b.rajh@shopware.com>
2023-05-05 17:00:22 +0200

    chore: release 1.0.0-alpha.5

--- __tests__/unit/buildSiderbarNav.test.ts
+++ __tests__/unit/buildSiderbarNav.test.ts
@@ -35,29 +35,35 @@ describe('builds sidebar', async () => {
             "        {\n" +
             "          \"link\": \"/foo/bar-With-small_API-custom-with-nav-heading.html\",\n" +
             "          \"text\": \"Custom title\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar-With-small_API-custom.html\",\n" +
             "          \"text\": \"Custom title\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar-With-small_API.html\",\n" +
             "          \"text\": \"Bar With small API\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar3.html\",\n" +
             "          \"text\": \"Bar3\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/foo4.html\",\n" +
             "          \"text\": \"Foo4\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        }\n" +
-            "      ]\n" +
+            "      ],\n" +
+            "      \"collapsed\": null\n" +
             "    },\n" +
             "    {\n" +
             "      \"link\": \"/foo/bar/\",\n" +
@@ -66,40 +72,47 @@ describe('builds sidebar', async () => {
             "        {\n" +
             "          \"text\": \"Bar1 custom title\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-both.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1 custom heading\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-heading.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1 custom title\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-title.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1\",\n" +
             "          \"link\": \"/foo/bar/bar1.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Foo2\",\n" +
             "          \"link\": \"/foo/bar/foo2.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        }\n" +
-            "      ]\n" +
+            "      ],\n" +
+            "      \"collapsed\": null\n" +
             "    },\n" +
             "    {\n" +
-            "      \"link\": \"#\",\n" +
             "      \"text\": \"No index\",\n" +
             "      \"items\": [\n" +
             "        {\n" +
             "          \"text\": \"File\",\n" +
             "          \"link\": \"/foo/no-index/file.html\",\n" +
-            "          \"items\": []\n" +
+            "          \"items\": [],\n" +
+            "          \"collapsed\": null\n" +
             "        }\n" +
-            "      ]\n" +
+            "      ],\n" +
+            "      \"collapsed\": null\n" +
             "    }\n" +
             "  ]\n" +
             "}"
