3586fecf1d6702d0ed7d2c3ffb7a20bad26987f6
Bojan Rajh <b.rajh@shopware.com>
2023-05-09 14:53:03 +0200

    Refactor style

--- src/shopware/components/SwagSidebarVersionSwitcher.vue
+++ src/shopware/components/SwagSidebarVersionSwitcher.vue
@@ -1,16 +1,28 @@
 <template>
   <div v-if="Object.keys(versions).length" class="SwagSidebarVersionSwitcher">
-    Version:
+    <!-- Version: -->
     <select
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="SwagSidebarVersionSwitcher_select"
         v-model="selectedVersion">
       <option v-for="(title, value) in versions" :value="value">{{ title }}</option>
     </select>
   </div>
 </template>
 
+<style lang="scss">
+.SwagSidebarVersionSwitcher {
+  @apply mb-4 lg:mb-0;
+  &_select {
+    @apply bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5;
+    @apply focus:ring-blue-500 focus:border-blue-500;
+    @apply dark:bg-#272727 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white;
+    @apply dark:focus:ring-blue-500 dark:focus:border-blue-500
+  }
+}
+</style>
+
 <script setup lang="ts">
-import {ref, watch} from "vue";
+import {ref, watch, computed} from "vue";
 import {useConfig} from "../composables/config";
 import {useRoute, useRouter} from "vitepress";
 import {flattenSidebar} from "../support/sidebar";
@@ -23,9 +35,9 @@ const sidebarConfig = config.value.sidebar;
 
 const versionSwitcherConfig = config.value?.swag?.versionSwitcher ?? {};
 const versionPaths = versionSwitcherConfig?.paths ?? [];
-const versions = versionPaths.find(paths => Object.keys(paths).find(key => route.path.startsWith(`/${key}`))) ?? {};
+const versions = computed(() => versionPaths.find(paths => Object.keys(paths).find(key => route.path.startsWith(`/${key}`))) ?? {});
 
-const versionMatch = Object.keys(versions ?? {})
+const versionMatch = Object.keys(versions.value)
     .filter(key => route.path.startsWith(`/${key}`))
     .sort((a, b) => {
       if (a.length > b.length) {
