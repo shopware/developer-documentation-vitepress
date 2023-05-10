417d5d6dc2606bbe3a9058baffc347def5ffce1d
Bojan Rajh <b.rajh@shopware.com>
2023-05-10 17:32:58 +0200

    Ellipsis on breadcrumbs

--- src/shopware/components/SwagBreadcrumbs.vue
+++ src/shopware/components/SwagBreadcrumbs.vue
@@ -1,38 +1,24 @@
 <template>
     <nav class="SwagBreadcrumbs hidden lg:flex mb-10" aria-label="Breadcrumb"
          v-if="breadcrumbs.length > 1">
-        <ol class="inline-flex items-center space-x-1 md:space-x-3">
-            <!--<li class="inline-flex items-center">
-                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                    <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                    Home
-                </a>
-            </li>-->
-            <li v-for="(breadcrumb, i) in breadcrumbs">
-                <div class="flex items-center">
-                    <svg v-if="i > 0" aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor"
-                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                              clip-rule="evenodd"></path>
-                    </svg>
-                    <span v-if="!breadcrumb.link"
-                          class="SwagBreadcrumbs__span">{{
-                        breadcrumb.text
-                        }}</span>
-                    <a v-else
-                       :href="breadcrumb.link"
-                       :class="[breadcrumb.color ? 'SwagBreadcrumbs__button' : 'SwagBreadcrumbs__link', breadcrumb.color]">{{
-                        breadcrumb.text
-                        }}</a>
-                </div>
+        <ol class="SwagBreadcrumbs_ol">
+            <li v-for="(breadcrumb, i) in breadcrumbs" class="SwagBreadcrumbs_li">
+                <svg v-if="i > 0" aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor"
+                     viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"></path>
+                </svg>
+                <span v-if="!breadcrumb.link"
+                      class="SwagBreadcrumbs__span">{{
+                    breadcrumb.text
+                    }}</span>
+                <a v-else
+                   :href="breadcrumb.link"
+                   :class="[breadcrumb.color ? 'SwagBreadcrumbs__button' : 'SwagBreadcrumbs__link', breadcrumb.color]">{{
+                    breadcrumb.text
+                    }}</a>
             </li>
-            <!--<li aria-current="page">
-                <div class="flex items-center">
-                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                    <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400" :class="breadcrumb.color">{{ breadcrumb.title }}</span>
-                </div>
-            </li>-->
         </ol>
     </nav>
 </template>
@@ -43,6 +29,18 @@
     @apply ml-1 md:ml-2;
   }
 
+  &_ol {
+    @apply inline-flex items-center space-x-1 md:space-x-3;
+    max-width: 100%;
+  }
+
+  &_li {
+    @apply flex items-center;
+    &:nth-child(n+4) {
+      min-width: 0;
+    }
+  }
+
   &__span,
   &__link {
     @apply text-sm font-medium text-gray-700;
@@ -51,6 +49,9 @@
 
   &__link {
     @apply hover:text-blue-600;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   &__button {
