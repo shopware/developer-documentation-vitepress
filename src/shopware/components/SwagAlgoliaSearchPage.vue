f1636049d8d281b36de1f7a8fe25e84bc4d20689
Bojan Rajh <b.rajh@shopware.com>
2023-05-25 15:56:37 +0200

    Refinement

--- src/shopware/components/SwagAlgoliaSearchPage.vue
+++ src/shopware/components/SwagAlgoliaSearchPage.vue
@@ -1,46 +1,47 @@
 <template>
   <AisInstantSearch class="SwagAlgoliaSearchPage" :index-name="indexName" :search-client="searchClient">
-    <div class="flex">
+    <div class="SwagAlgoliaSearchPage_sidebar">
       <AisSearchBox placeholder="Search here…" class="searchbox"/>
-      <select>
-        <option value="main">latest</option>
-        <option value="v6.4">v6.4</option>
-        <option value="v6.4">v6.3</option>
-      </select>
+
+      <!--<AisMenu attribute="version"/>-->
+
+      <!--<AisMenu attribute="repository"/>-->
+
+      <!--<AisRefinementList attribute="version"/>-->
+
+      <AisHierarchicalMenu :attributes="['version', 'hierarchy.lvl0']"/>
+
+      <!--<AisCurrentRefinements/>-->
+
+      <!--<AisHitsPerPage />-->
     </div>
 
-    <AisStats/>
+    <div class="SwagAlgoliaSearchPage_content">
 
-    <AisMenu attribute="version"/>
+      <AisStats/>
 
-    <AisRefinementList attribute="version"/>
+      <!--<AisBreadcrumb :attributes="['lvl0', 'lvl1', 'version', 'repository']"/>-->
 
-    <AisHierarchicalMenu :attributes="['version']" />
+      <AisHits>
+        <template v-slot="{ items, sendEvent }">
 
-    <AisCurrentRefinements />
+          <div class="SwagAlgoliaSearchPage_list">
+            <a :href="item.url" class="SwagAlgoliaSearchPage_item c-any-card" v-for="item in items"
+               :key="item.objectID">
+              <span class="SwagAlgoliaSearchPage_title">
+                {{ item.hierarchy.lvl2 || item.hierarchy.lvl1 || item.hierarchy.lvl0 }}
+              </span>
+              <span class="SwagAlgoliaSearchPage_sub">{{ item.url }}</span>
+              <AisHighlight :hit="item" attribute="repository"/>
+              <AisHighlight :hit="item" attribute="version"/>
+            </a>
+          </div>
+        </template>
+      </AisHits>
 
-    <AisBreadcrumb :attributes="['lvl0', 'lvl1', 'version', 'repository']"/>
-
-    <!--<AisHitsPerPage />-->
-
-    <AisHits>
-      <template v-slot="{ items, sendEvent }">
-        <div class="SwagAlgoliaSearchPage_list">
-          <a :href="item.url" class="SwagAlgoliaSearchPage_item c-any-card" v-for="item in items"
-             :key="item.objectID">
-                            <span class="SwagAlgoliaSearchPage_title">{{
-                                item.hierarchy.lvl2 || item.hierarchy.lvl1 || item.hierarchy.lvl0
-                              }}</span>
-            <span class="SwagAlgoliaSearchPage_sub">{{ item.url }}</span>
-            <AisHighlight :hit="item" attribute="description"/>
-            <AisHighlight :hit="item" attribute="name"/>
-          </a>
-        </div>
-      </template>
-    </AisHits>
-
-    <div class="pagination">
-      <AisPagination/>
+      <div class="pagination">
+        <AisPagination/>
+      </div>
     </div>
 
     <!--<AisConfigure :hits-per-page.camel="20" :filters="`version:main`" />-->
@@ -49,7 +50,19 @@
 
 <script setup lang="ts">
 import {
-  AisInstantSearch, AisSearchBox, AisHits, AisHighlight, AisStats, AisRefinementList, AisConfigure, AisHierarchicalMenu, AisMenu, AisCurrentRefinements, AisHitsPerPage, AisBreadcrumb, AisPagination 
+  AisInstantSearch,
+  AisSearchBox,
+  AisHits,
+  AisHighlight,
+  AisStats,
+  AisRefinementList,
+  AisConfigure,
+  AisHierarchicalMenu,
+  AisMenu,
+  AisCurrentRefinements,
+  AisHitsPerPage,
+  AisBreadcrumb,
+  AisPagination
 } from 'vue-instantsearch/vue3/es';
 import algoliasearch from 'algoliasearch/lite';
 import 'instantsearch.css/themes/algolia-min.css';
@@ -71,8 +84,25 @@ const searchClient = algoliasearch(appId, apiKey);
   margin: 0 auto;
 }
 
+.ais-HierarchicalMenu-count {
+  @apply ml-2;
+}
+
 .SwagAlgoliaSearchPage {
-  @apply grid gap-4 my-6;
+  @apply grid md:flex gap-10 my-6;
+
+  &_sidebar {
+    @apply md:w-56 md:flex-none;
+    @apply grid gap-4;
+    @apply self-start;
+  }
+
+  &_content {
+    @apply md:flex-1 md:min-w-0;
+    @apply grid gap-4;
+    @apply self-start;
+  }
+
   &_list {
     @apply grid gap-4;
   }
