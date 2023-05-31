8a4866f9347d2a18cf0d3660edfccd94f73b5df3
Bojan Rajh <b.rajh@shopware.com>
2023-05-31 16:06:38 +0200

    Update 404

--- src/shopware/components/override/NotFound.vue
+++ src/shopware/components/override/NotFound.vue
@@ -5,7 +5,8 @@ import SwagRelatedArticles from "../SwagRelatedArticles.vue";
 <template>
   <div class="NotFound flex flex-col gap-8 content-center items-center justify-center mt-24 max-w-800px mx-auto px-8">
 
-    <h1 class="accent text-3xl">PAGE NOT FOUND</h1>
+    <h1 class="accent text-5xl">404</h1>
+    <h2 class="text-2xl">PAGE NOT FOUND</h2>
 
     <blockquote class="quote">
       But if you don't change your direction, and if you keep looking, you may end up where you are heading.
