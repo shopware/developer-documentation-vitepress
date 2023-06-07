a12ae7e7e62f0340dea4228704077ddd738a1c1d
Bojan Rajh <b.rajh@shopware.com>
2023-06-07 15:56:52 +0200

    Typography style

--- src/shopware/components/SwagLandingCardList.vue
+++ src/shopware/components/SwagLandingCardList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="SwagLandingCardListWrapper relative flex flex-col gap-4 leading-7">
+  <div class="SwagLandingCardListWrapper relative flex flex-col gap-4 leading-7 py-10">
     <h3 v-if="$slots.title"><slot name="title"></slot></h3>
     <slot v-if="$slots.description" name="description"></slot>
-    <div class="gap-10 SwagLandingCardList pt-5 pb-10 grid sm:grid-cols-2 md:grid-cols-3">
+    <div class="gap-10 SwagLandingCardList grid sm:grid-cols-2 md:grid-cols-3 pt-5">
       <slot name="cards">
         <SwagLandingCard v-for="item in exposed" v-bind="item"/>
       </slot>
