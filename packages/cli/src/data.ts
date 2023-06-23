51010785f824028da191215a14be86d86a0caa60
Bojan Rajh <b.rajh@shopware.com>
2023-06-23 09:56:19 +0200

    Extract portal.json config

--- packages/cli/src/data.ts
+++ packages/cli/src/data.ts
@@ -1,86 +1,54 @@
 import {env} from "process";
 import path from "path";
+import fs from "fs";
+import {output} from "./output";
 
-export const docsSrcDir = 'src';
+export interface Repository {
+    name: string;
+    src: string | string[];
+    dst: string | string[];
+    branch: string | string[];
+    org: string | string[];
+    env?: object;
+    skip?: boolean;
+}
 
-export const repositories = [
-    {
-        name: 'shopware/docs',
-        src: '.',
-        dst: 'docs',
-        branch: env.BRANCH_DOCS || 'next' || 'main',
-        org: env.ORG_DOCS || 'shopware',
-    },
-    {
-        name: 'shopware/docs',
-        src: '.',
-        dst: path.join('docs', 'v6.4'),
-        branch: env.BRANCH_DOCS_64 || 'next-6.4',
-        org: env.ORG_DOCS || 'shopware',
-    },
-    {
-        name: 'shopware/docs',
-        src: '.',
-        dst: path.join('docs', 'v6.3'),
-        branch: env.BRANCH_DOCS_63 || 'next-6.3',
-        org: env.ORG_DOCS || 'shopware',
-    },
-    {
-        name: 'shopware/frontends',
-        src: path.join('apps', 'docs', 'src'),
-        dst: 'frontends',
-        branch: env.BRANCH_FRONTENDS || 'main',
-        org: env.ORG_FRONTENDS || 'shopware',
-        skip: true,
-    },
-    {
-        name: 'gitlab.shopware.com/product/engineering/platform-group/pwa/frontends',
-        src: path.join('apps', 'docs', 'src'),
-        dst: 'frontends-gl',
-        branch: env.BRANCH_FRONTENDS || 'main',
-        org: env.ORG_FRONTENDS || 'shopware',
-        env: {
-            GITLAB_FRONTENDS_USERNAME: {
-                as: 'user',
-                description: 'GitLab deploy key username'
-            },
-            GITLAB_FRONTENDS_ACCESS_KEY: {
-                as: 'pass',
-                description: 'GitLab deploy key'
-            },
-        },
-        skip: true,
-    },
-    {
-        name: 'shopware/admin-extension-sdk',
-        src: path.join('docs', 'docs', 'guide'),
-        dst: 'resources/admin-extension-sdk',
-        branch: env.BRANCH_ADMIN_EXTENSION_SDK || 'DX-223' || 'main',
-        org: env.ORG_ADMIN_EXTENSION_SDK || 'shopware',
-        skip: true,
-    },
-    {
-        name: 'shopware/meteor-icon-kit',
-        src: 'docs',
-        dst: 'resources/meteor-icon-kit',
-        branch: env.BRANCH_METEOR_ICON_KIT || 'DX-223' || 'main',
-        org: env.ORG_METEOR_ICON_KIT || 'bojanrajh',
-        env: {
-            FIGMA_FILE: {
-                description: 'Figma file ID'
-            },
-            FIGMA_TOKEN: {
-                description: 'Figma API key'
-            },
-        },
-        skip: true,
-    },
-    {
-        name: 'shopware/meteor-component-library',
-        src: 'docs',
-        dst: 'resources/meteor-component-library',
-        branch: env.BRANCH_METEOR_COMPONENT_LIBRARY || 'DX-231' || 'main',
-        org: env.ORG_METEOR_COMPONENT_LIBRARY || 'bojanrajh',
-        skip: true,
+let portalJson: { repositories: Repository[], src?: string } | null = null;
+const getPortalJson = () => {
+    if (portalJson) {
+        return portalJson;
     }
-];
+
+    const portalJsonPath = path.join('.', '.vitepress', 'portal.json');
+    output.debug('Reading ' + portalJsonPath);
+
+    return portalJson = JSON.parse(`${fs.readFileSync(portalJsonPath)}`)
+}
+
+const fetchSrc = () => getPortalJson()?.src ?? 'src';
+
+const mappedRepositories = () => (portalJson?.repositories || [])
+    .map((repo: Repository) => {
+        if (Array.isArray(repo.branch)) {
+            repo.branch = repo.branch
+                .map(key => key.startsWith('env.') ? env[key.substring('env.'.length)] : key)
+                .find(value => value?.length)!;
+        }
+        if (Array.isArray(repo.org)) {
+            repo.org = repo.org
+                .map(key => key.startsWith('env.') ? env[key.substring('env.'.length)] : key)
+                .find(value => value?.length)!;
+        }
+        if (Array.isArray(repo.src)) {
+            repo.src = path.join(...repo.src);
+        }
+        if (Array.isArray(repo.dst)) {
+            repo.dst = path.join(...repo.dst);
+        }
+
+        return repo;
+    });
+
+export const docsSrcDir = fetchSrc();
+
+export const repositories = mappedRepositories();
