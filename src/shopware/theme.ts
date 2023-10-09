58159d3806734c56d85d4138c97cf4f7c2a6c457
Bojan Rajh <b.rajh@shopware.com>
2023-10-09 13:06:43 +0200

    fix: reload MediumZoom on route change

--- src/shopware/theme.ts
+++ src/shopware/theme.ts
@@ -120,15 +120,11 @@ const SWAGTheme = (myConfig: { enhanceApp?: Function, slots?: {[key: string]:any
         // https://github.com/vuejs/vitepress/issues/854
         // https://github.com/francoischalifour/medium-zoom/issues/184
         const route = useRoute();
-        const initZoom = () => {
-            mediumZoom('.main img', {background: 'var(--vp-c-bg)'});
-        };
-        onMounted(() => {
-            initZoom();
-        });
+        const initZoom = () => mediumZoom('.main img', {background: 'var(--vp-c-bg)'});
+        onMounted(() => initZoom());
         watch(
             () => route.path,
-            () => nextTick(() => mediumZoom.refresh())
+            () => nextTick(() => initZoom())
         );
     },
 });
