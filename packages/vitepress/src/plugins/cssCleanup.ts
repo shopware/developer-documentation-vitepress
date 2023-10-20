d6ce0b03076575cbed6b32b9ef086472d6af1266
Bojan Rajh <b.rajh@shopware.com>
2023-10-20 10:51:55 +0200

    feat: rework cleanup, base cleanup

--- packages/vitepress/src/plugins/cssCleanup.ts
+++ packages/vitepress/src/plugins/cssCleanup.ts
@@ -1,26 +1,67 @@
 import type {Plugin} from "vite";
 import postcss from "postcss";
 
+export const baseCleanup = [
+    {
+        scope: '/node_modules/vitepress/dist/client/theme-default/',
+        file: '/vp-doc.css',
+        cleanup: [
+            '.vp-doc h2',
+            '.vp-doc hr',
+            '.vp-doc a',
+            '.vp-doc h1 .header-anchor',
+            '.vp-doc h2 .header-anchor',
+            '.vp-doc h3 .header-anchor',
+            '.vp-doc h4 .header-anchor',
+            '.vp-doc h5 .header-anchor',
+        ]
+    },
+    {
+        scope: '/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue',
+        file: '&lang.css',
+        cleanup: [
+            /\.VPSocialLink > :deep\(svg\)/,
+        ]
+    },
+];
+
 export function CssCleanup(options = {cleanup: []}): Plugin {
     return {
         name: "shopware-css-specificity-cleanup",
         enforce: "pre",
         async transform(code, id) {
+            // skip non-css files
             if (!id.match(/\.css\b/)) return null;
-            if (!id.includes('/node_modules/vitepress/dist/client/theme-default/')) return null;
-            if (!id.endsWith('/vp-doc.css')) return null;
 
+            const foundScope = options.cleanup.find(clean => id.includes(clean.scope) && id.endsWith(clean.file));
+            if (!foundScope) {
+                // skip out-of-scope files
+                return null;
+            }
+
+            console.log(`Pre-processing ${id}`, code);
             const {root} = await postcss().process(code, {
                 from: undefined,
             });
 
             // Remove selectors
-            options.cleanup.forEach((selector) => {
+            foundScope.cleanup.forEach((selector) => {
                 root.nodes = root.nodes.filter(node => {
+                    console.log(node.selector);
+                    if (!node.selector) {
+                        return true;
+                    }
+
                     // remove by selector name
                     if (node.selector === selector) {
-                        console.log(`Removing ${selector}`)
+                        console.log(`Removing ${selector}`);
                         return false;
+                    } else if (selector instanceof RegExp) {
+                        const toRemove = !!node.selector.match(selector);
+                        if (toRemove) {
+                            console.log(`Removing ${selector}`);
+                            return false;
+                        }
                     }
 
                     return true;
