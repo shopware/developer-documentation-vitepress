c4e8bcfe69f4b5b08c87e4d010d56a2f17e697a3
Bojan Rajh <b.rajh@shopware.com>
2023-11-23 15:50:10 +0100

    fix: tests

--- __tests__/unit/buildSiderbarNav.test.ts
+++ __tests__/unit/buildSiderbarNav.test.ts
@@ -36,39 +36,39 @@ describe('builds sidebar', async () => {
             "          \"link\": \"/foo/bar-With-small_API-custom-with-nav-heading.html\",\n" +
             "          \"text\": \"Custom title\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar-With-small_API-custom.html\",\n" +
             "          \"text\": \"Custom title\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar-With-small_API.html\",\n" +
             "          \"text\": \"Bar With small API\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/bar3.html\",\n" +
             "          \"text\": \"Bar3\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"link\": \"/foo/foo4.html\",\n" +
             "          \"text\": \"Foo4\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        }\n" +
             "      ],\n" +
-            "      \"collapsed\": null\n" +
+            "      \"collapsed\": null,\n" +
             "      \"meta\": {}\n" +
             "    },\n" +
             "    {\n" +
@@ -79,39 +79,39 @@ describe('builds sidebar', async () => {
             "          \"text\": \"Bar1 custom title\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-both.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1 custom heading\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-heading.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1 custom title\",\n" +
             "          \"link\": \"/foo/bar/bar1-custom-title.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Bar1\",\n" +
             "          \"link\": \"/foo/bar/bar1.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        },\n" +
             "        {\n" +
             "          \"text\": \"Foo2\",\n" +
             "          \"link\": \"/foo/bar/foo2.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        }\n" +
             "      ],\n" +
-            "      \"collapsed\": null\n" +
+            "      \"collapsed\": null,\n" +
             "      \"meta\": {}\n" +
             "    },\n" +
             "    {\n" +
@@ -121,11 +121,11 @@ describe('builds sidebar', async () => {
             "          \"text\": \"File\",\n" +
             "          \"link\": \"/foo/no-index/file.html\",\n" +
             "          \"items\": [],\n" +
-            "          \"collapsed\": null\n" +
+            "          \"collapsed\": null,\n" +
             "          \"meta\": {}\n" +
             "        }\n" +
             "      ],\n" +
-            "      \"collapsed\": null\n" +
+            "      \"collapsed\": null,\n" +
             "      \"meta\": {}\n" +
             "    }\n" +
             "  ]\n" +
