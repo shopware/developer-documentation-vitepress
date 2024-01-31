c698fa78e1ce9a248e650d4e3d58ab411e9c8ebe
Bojan Rajh <b.rajh@shopware.com>
2024-01-31 13:58:30 +0100

    fix: ref

--- src/shopware/components/copilot/ml.ts
+++ src/shopware/components/copilot/ml.ts
@@ -13,7 +13,7 @@ export const qa = (initialCollection) => {
     let stopping = ref(false);
 
     let collection = ref(initialCollection);
-    const setCollection = newCollection => collection = newCollection;
+    const setCollection = newCollection => collection.value = newCollection;
 
     let requestAnswer = () => {
         pending.value = true;
