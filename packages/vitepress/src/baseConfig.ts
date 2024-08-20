405c48634e909c004242900c2a335a1be3170691
Bojan Rajh <b.rajh@shopware.com>
2024-08-20 16:58:22 +0200

    fix: typo

--- packages/vitepress/src/baseConfig.ts
+++ packages/vitepress/src/baseConfig.ts
@@ -104,15 +104,15 @@ export default async () => ({
       "script",
       {},
       `;(() => {
-        const saved = localStorage.getItem('vue-theme-appearance')
-        if (
-          !saved || saved === 'auto'
-            ? window.matchMedia(`(prefers-color-scheme: dark)`).matches
-            : saved === 'dark'
-        ) {
-          document.documentElement.classList.add('dark')
-        }
-      })()`
+  const saved = localStorage.getItem('vue-theme-appearance')
+  if (
+    !saved || saved === 'auto'
+      ? window.matchMedia('(prefers-color-scheme: dark)').matches
+      : saved === 'dark'
+  ) {
+    document.documentElement.classList.add('dark')
+  }
+})()`
     ],
   ],
 
